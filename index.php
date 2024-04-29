a4514539b76e4b44866fb4c4daf43c2fcaca44cf
chen-chan1 <161545719+chen-chan1@users.noreply.github.com>
2024-04-29 09:13:13 -0500

    update index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=0.47">
+  <title>uploader</title>
+  </head>
+  <body>
+    <center>
+      <img src="static/title.png" alt="logo" height="100" width="384"><br>
+      <small><br>
+      this is a file sharing website.<br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <table border="1"><tbody><tr><td>
+        Upload Boards:<br><a href="warota.php">Everything</a><br>
+      </td></tr></tbody></table>
+      <p class="footerinfo">
+        <a href="https://github.com/Heyuri/Uploader">Uploader</a>
+      </p>
+    </center>
+  </body>
+</html>

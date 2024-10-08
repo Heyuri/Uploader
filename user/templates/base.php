48e3ed89e8698718569d4b21e073d0a8c10bfbc6
kaguy4 <kaguya@kolyma.net>
2024-10-08 18:48:49 +0300

    fix empty password bug for user boards

--- user/templates/base.php
+++ user/templates/base.php
@@ -1018,19 +1018,20 @@ function userDeletePost(){
     $password = $_POST['password'];
 
     $postData = getDataByID($fileID);
+    
+    if(empty($password)) drawErrorPageAndExit('Deletion Error', "The password you entered was blank.");
+    
     if(is_null($postData)){
         drawErrorPageAndExit('Deletion Error','The file cannot be found.');
-    }
-    elseif($password == getPassword($postData) || $password == $conf['adminPassword'] || $password == SUPERADMINPASS){
-	deleteDataFromLogByID($fileID);
-	$thumbPath = $conf['thumbDir'] . $conf['prefix'] . getID($postData) . '_thumb.' . getFileExtension($postData);
-	unlink($thumbPath);
-
-        drawMessageAndRedirectHome('file has been deleted.','If this page dose not change, click "Back".');
-    }
-    elseif(getPassword($postData) == ''){
+    } elseif(getPassword($postData) == ''){
         drawErrorPageAndExit('Deletion Error','There was not a password when this post was created. Contact the administrator to request deletion');
-    }else{
+    } elseif($password === getPassword($postData) || $password === $conf['adminPassword']){
+		deleteDataFromLogByID($fileID);
+		$thumbPath = $conf['thumbDir'] . $conf['prefix'] . getID($postData) . '_thumb.' . getFileExtension($postData);
+		unlink($thumbPath);
+
+        drawMessageAndRedirectHome('file has been deleted.','If this page does not change, click "Back".');
+    } else{
         drawErrorPageAndExit('Deletion Error','The password is incorrect.');
     }
 }
@@ -1072,22 +1073,8 @@ if(isset($_GET['goingto'])){
             drawHeader();
             drawSettingsForm();
             drawFooter();
-            die(); 
-	case "ownersettings":
-	    drawHeader();
-    	    drawOwnerForm(); 
-	    drawFooter();
-	    die();
-	case "edit":
-	    handleBoardEdit();
-	    die();
-	case "delete":
-	    drawHeader();
-	    drawBoardDeletionForm();
-	    drawFooter();
-	    die();
+            die();
     }
-
 }
 if(isset($_GET['page'])){
     $page = $_GET['page'];

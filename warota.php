3129c40ec7c7deb89b17cc995aeca538ee5d2544
kaguy4 <kaguya@kolyma.net>
2026-07-14 18:08:19 +0300

    note 4.3 changes

--- warota.php
+++ warota.php
@@ -60,6 +60,7 @@
 	2026/03/08 v4.0 Major code refactor, added chunked uploads, and various other features and fixes.
 	2026/03/16 v4.1 Misc improvements including some front-end updates, Japanese language option added. A homepage for the software is created.
 	2026/03/26 v4.2 Minor tweaks to table HTML and CSS.
+	2026/07/07 v4.3 AJAX file uploading without a full page refresh, track own uploaded files with a button to copy their links to clipboard.
 ■Installation
 	・Clone repo into web directory (or unzip it there)
 	・cd into the directory and do this: "chmod +x prepare.sh", then run it with "./prepare.sh"
